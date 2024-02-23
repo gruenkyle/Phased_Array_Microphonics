c0b5264860fc3e86b8855b960761ca462d20c551
Kyle Gruen <kylegruen4@gmail.com>
2024-02-22 19:53:03 -0500

    Final Commit 0222024

--- Phased_Array_Python/main.py
+++ Phased_Array_Python/main.py
@@ -9,14 +9,14 @@ import numpy as np
 
 # FIELD VARIABLES # 
 FS = 44100 # AVG number of samples obtained per second (Sample Rate)
-TOTAL_SPREAD = 13.716 # Total Spread of Phased Array from Mic 1 -> Mic N
+TOTAL_SPREAD = 15.24 # Total Spread of Phased Array from Mic 1 -> Mic N
 MIC_COUNT = 3 # Total Number of Microphones 
 SCALAR = TOTAL_SPREAD / (MIC_COUNT - 1) # Scalar Multiple For Equidistant Microphones
 
-code = 2004
+code = 110300
 
-SYS_ADJ_DIST = 7.625 # Adjacent Distance from Left Most Microphone
-SYS_OPP_DIST = 6.858 #Opposite Distance from Left Most Microphone
+SYS_ADJ_DIST = 7.62 # Adjacent Distance from Left Most Microphone
+SYS_OPP_DIST =  #Opposite Distance from Left Most Microphone
 
 FORLOOPARR = np.arange(MIC_COUNT) #Looping Array for Mics
 
